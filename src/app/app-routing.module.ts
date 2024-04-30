f3243e755a1f684d830e144e9158b0a9f203c3c6
erli1a <erikaachote15@gmail.com>
2024-04-29 23:00:49 -0500

    museos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MuseumsModule } from './page-museum/museums/museums.module';
+import { PageMuseumModule } from './page-museum/page-museum.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path:'page-museum',
+  loadChildren: ()=> import('./page-museum/page-museum.module').then(m=>PageMuseumModule)
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
