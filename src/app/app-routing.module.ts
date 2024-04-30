7980c729b2a543ef44293cb5d4c2f96ade5320e1
fe222004 <fernanda.899.andrade@gmail.com>
2024-04-30 12:50:26 -0500

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+  {
+    path:'music',
+    loadChildren: () => import('./page-music/page-music.module').then(m => m.PageMusicModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
