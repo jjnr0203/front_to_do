933b8163dee3b58b49db6ec2a5c25115500cc1db
AdrianReyes555 <len555rey@gmail.com>
2024-05-01 07:37:24 -0500

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageVehiculosComponent } from './page-vehiculos/page-vehiculos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageVehiculosComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/page-vehiculos/page-vehiculos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-vehiculos',
+  templateUrl: './page-vehiculos.component.html',
+  styleUrl: './page-vehiculos.component.scss'
+})
+export class PageVehiculosComponent {
+
+}
