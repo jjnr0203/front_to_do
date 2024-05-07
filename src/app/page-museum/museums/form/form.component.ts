bfcf42eeb4aba0ef751c8807377592e8ff4ab981
erli1a <erikaachote15@gmail.com>
2024-05-06 22:53:24 -0500

    museums

--- src/app/page-museum/museums/form/form.component.ts
+++ src/app/page-museum/museums/form/form.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+  styleUrls: ['./form.component.scss']
 })
 export class FormComponent {
+  private formBuilder: FormBuilder = inject(FormBuilder)
 
+  protected form: FormGroup;
+
+  constructor(){
+    this.form = this.formBuilder.group({
+      name: [null, Validators.required, Validators.minLength(3), Validators.maxLength(10)],
+      lastname: [null, Validators.required, Validators.minLength(10), Validators.maxLength(20)],
+      numberofrooms: [null, Validators.required],
+      years: [null, Validators.required],
+      petfriendly: [null, Validators.required],
+    })
+  }
+
+  validateForm() {
+    if (this.form.valid) {
+      alert('Válido');
+    } else {
+      alert('No válido');
+    }
+  }
 }
