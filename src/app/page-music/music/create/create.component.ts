d94fab9d90ad2a95ea3af60729e948e05c84ada4
fe222004 <fernanda.899.andrade@gmail.com>
2024-05-10 14:11:18 -0500

    primeNg and controls

--- src/app/page-music/music/create/create.component.ts
+++ src/app/page-music/music/create/create.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import {
+  AbstractControl,
   FormBuilder,
   FormControl,
   FormGroup,
@@ -13,8 +14,8 @@ import {
 })
 export class CreateComponent {
   private formBuilder = inject(FormBuilder);
- //protected nameControl: FormControl = new FormControl<string>('');
- //protected artistaControl: FormControl = new FormControl<string>('');
+  //protected nameControl: FormControl = new FormControl<string>('');
+  //protected artistaControl: FormControl = new FormControl<string>('');
 
   protected form: FormGroup;
 
@@ -25,14 +26,39 @@ export class CreateComponent {
   get buildForm(): FormGroup {
     return (this.form = this.formBuilder.group({
       name: ['', [Validators.required, Validators.minLength(2)]],
-      artista: ['', [Validators.required, Validators.minLength(2)]],
-      descripcion: ['', [Validators.required, Validators.minLength(10)]],
-      date:['',[Validators.required]],
-      num:['', [Validators.required, Validators.pattern("^[0-9]*$")]],
-      esSingle : [false]        
+      artist: ['', [Validators.required, Validators.minLength(2)]],
+      description: ['', [Validators.required, Validators.minLength(10)]],
+      date: ['', [Validators.required]],
+      num: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      esSingle: [false],
     }));
   }
 
+  get name(): AbstractControl {
+    return this.form.controls['name'];
+  }
+
+  get artist(): AbstractControl {
+    return this.form.controls['artist'];
+  }
+
+  get description(): AbstractControl {
+    return this.form.controls['description'];
+  }
+
+  get date(): AbstractControl {
+    return this.form.controls['date'];
+  }
+
+  get num(): AbstractControl {
+    return this.form.controls['num'];
+  }
+
+  get esSingle(): AbstractControl {
+    return this.form.controls['esSingle'];
+  }
+
+  
   validateForm() {
     if (this.form.valid) {
       alert('valido');
